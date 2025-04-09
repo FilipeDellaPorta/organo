b973315d477b5f0560f5058591be90eab86e2aaa
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-09 12:07:37 -0300

    feat: :sparkles: add: componente do card do colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,20 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img
+          src='https://github.com/FilipeDellaPorta.png'
+          alt='foto do colaborador Filipe Della Porta'
+        ></img>
+      </div>
+      <div className='rodape'> 
+        <h4>Filipe</h4>
+        <h5>Front-End</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
